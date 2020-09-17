734fe59101516d7a461afd559eb0f123dd7741df
zhoufan170 <32598414+zhoufan170@users.noreply.github.com>
2020-09-17 19:06:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # 启动celery
-nohup celery -A wcl_analysis worker -l info -Q wcl_analysis > /tmp/wcl_celery.log
+sudo systemctl restart wclcelery
 # 启动web
-nohup python3 manage.py runserver 0.0.0.0:8080 > /tmp/wcl_web.log
+sudo systemctl restart wclweb
+# 启动jupyter notebook(运行脚本用)
+sudo systemctl restart jupyter
